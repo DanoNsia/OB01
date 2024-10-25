b7b14f7074334267feef4bea885d87ce12ea2683
DanoNsia <sibraevd1998@gmail.com>
2024-10-25 21:35:10 +0300

    main2 start

--- main2.py
+++ main2.py
@@ -21,3 +21,31 @@
 # 3. Протестировать методы:
 # Выбери один из созданных магазинов и протестируй все его методы: добавь товар, обнови цену, убери товар и запрашивай цену.
 
+class Store:
+    def __init__(self, name, address, items):
+        self.name = name
+        self.address = address
+        self.items = {
+    'apples': 0.5,
+    'bananas': 0.75,
+    'oranges': 0.65,
+    'grapes': 2.0,
+    'pears': 1.2
+}
+        self.adds = []
+
+    def adds(self, items):
+        add = Store(items)
+        self.adds.append(add)
+
+    def deletes(self, items):
+        delete = Store(items)
+        self.remove(delete)
+
+    def clears(self, items):
+        clear = Store(items)
+        self.clear(clear)
+
+    def prices(self, items):
+        price = Store(items)
+        self.get(price)
